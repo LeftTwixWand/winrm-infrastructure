ad0522ef65069b76d552814fb98db3ed4fb5d05e
Vladyslav Horbachov <vhorbachov@microsoft.com>
2023-12-13 17:19:18 +0100

    Add azure subscription reference

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@ resource "azurerm_resource_group" "rg" {
   location = "Germany West Central"
 }
 
+data "azurerm_subscription" "current" {}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "extensions-vnet"
   resource_group_name = azurerm_resource_group.rg.name
